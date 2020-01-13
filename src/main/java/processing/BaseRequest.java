fd57ac72f587e993fc91c1cf4bb4aa9f0e782e21
Andrianov Oleg <o.andrianov@pflb.ru>
2020-01-13 22:46:00 +0300

    Добавление библиотеки для работы с json и небольшие добавки в базовый запрос

--- src/main/java/processing/BaseRequest.java
+++ src/main/java/processing/BaseRequest.java
@@ -1,4 +1,14 @@
 package processing;
 
 public class BaseRequest {
+    private int code;
+
+    public BaseRequest(int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
 }
